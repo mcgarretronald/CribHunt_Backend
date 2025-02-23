31c2a76421ab0f6c4e68b6626c72533802c19114
mcgarret254 <mcgarretronald@gmail.com>
2025-02-24 01:30:00 +0300

    boilerplate action fix

--- cribhunt/settings.py
+++ cribhunt/settings.py
@@ -80,13 +80,14 @@ WSGI_APPLICATION = 'cribhunt.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "NAME": os.getenv("DB_NAME", ""),
+        "USER": os.getenv("DB_USER", ""),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"), 
+        "PORT": os.getenv("DB_PORT", "5432"), 
     }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
