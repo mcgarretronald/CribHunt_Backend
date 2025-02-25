c747d7edee6c470f3f74bc3f163845741ae44a1a
mcgarret254 <mcgarretronald@gmail.com>
2025-02-25 23:25:41 +0300

    fix

--- cribhunt/settings.py
+++ cribhunt/settings.py
@@ -89,22 +89,10 @@ TEMPLATES = [
 ]
 
 # DATABASE CONFIGURATION
-if DEBUG:
-    DATABASES = {
+DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("DB_NAME"),
-            "USER": os.getenv("DB_USER"),
-            "PASSWORD": os.getenv("DB_PASSWORD"),
-            "HOST": os.getenv("DB_HOST"),
-            "PORT": os.getenv("DB_PORT", "5432"),
         }
     }
 
